bea11ce4bc3be7ceeb72801b2a1cc47936c99a23
junyoo0305 <durortjs123@naver.com>
2025-07-24 12:17:43 +0900

    은행 프로그램

--- src/sec01/exam01/Bank.java
+++ src/sec01/exam01/Bank.java
@@ -3,17 +3,51 @@ package sec01.exam01;
 import java.util.Scanner;
 
 class BankAccount {
-	String name = "";
-	String account = "";
-	int balance = 0;
+	private String name = "";
+	private String account = "";
+	private int balance = 0;
 	
-	void deposit(int num) {
-		balance += num;
+	
+	
+	public BankAccount(String name, String account, int balance) {
+		this.name = name;
+		this.account = account;
+		this.balance = balance;
 	}
 	
-	void withdraw(int num) {
-		balance -= num;
+	void deposit(int balance) {
+		this.balance += balance;
 	}
+	
+	void withdraw(int balance) {
+		this.balance -= balance;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	
 }
 
 public class Bank {
@@ -22,12 +56,11 @@ public class Bank {
 		
 	boolean run = true;
 	boolean login = true;
-	boolean loop = true;
 	boolean menu = false;
 	int num = 0;
 	int count;
 	
-	BankAccount ba = new BankAccount();
+	BankAccount ba = new BankAccount("", "", 0);
 	
 	Scanner sc = new Scanner(System.in);
 	
@@ -46,7 +79,7 @@ public class Bank {
 				String str = sc.nextLine();
 				System.out.println("계정> ");
 				String str2 = sc.nextLine();
-				if (str.equals(ba.name) && str2.equals(ba.account)) {
+				if (str.equals(ba.getName()) && str2.equals(ba.getAccount())) {
 					System.out.println("로그인 성공");
 					login = false;
 					menu = true;
@@ -60,9 +93,9 @@ public class Bank {
 				System.out.println(" 회원가입 ");
 				System.out.println("--------------------------------");
 				System.out.println(" 회원가입할 이름 > ");
-				ba.name = sc.nextLine();
+				ba.setName(sc.nextLine());
 				System.out.println(" 회원가입할 계정 > ");
-				ba.account = sc.nextLine();
+				ba.setAccount(sc.nextLine());
 				break;
 			case 3:
 				run = false;
@@ -86,7 +119,7 @@ public class Bank {
 				break;
 			case 2:
 				System.out.println("출금액 >");
-				if (ba.balance < Integer.parseInt(sc.nextLine()) ) {
+				if (ba.getBalance() < Integer.parseInt(sc.nextLine()) ) {
 					System.out.println("잔고 보유액 보다 큰 액수는 출금 할 수 없습니다.");
 				} else {					
 					num = Integer.parseInt(sc.nextLine());
@@ -95,7 +128,7 @@ public class Bank {
 				break;
 			case 3:
 				System.out.println("잔고 >");
-				System.out.printf("%d", ba.balance);		
+				System.out.printf("%d", ba.getBalance());		
 				break;
 			case 4:
 				run = false;
